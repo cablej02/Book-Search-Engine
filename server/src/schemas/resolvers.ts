1e5606cf278e20a86e99e813df55776c6ef54ecf
Jason Cable <cablej02@gmail.com>
2025-02-13 01:28:17 -0600

    setup typeDefs and resolvers

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,8 +1,98 @@
+import { User } from '../models/index.js';
+import { signToken, AuthenticationError } from '../services/auth.js';
 
+interface User {
+    _id: string;
+    username: string;
+    email: string;
+    password: string;
+    bookCount: number;
+    savedBooks: Book[];
+}
+
+interface Book {
+    bookId: string;
+    authors: string[];
+    description: string;
+    title: string;
+    image: string;
+    link: string;
+}
+
+interface AddUserArgs {
+    input:{
+        username: string;
+        email: string;
+        password: string;
+    }
+}
+
+interface SaveBookArgs {
+    bookData: Book;
+}
+
+interface RemoveBookArgs {
+    bookId: string;
+}
+
+interface Context {
+    user?: User;
+}
 
 const resolvers = {
     Query: {
-        hello: () => 'Hello world!',
+        me: async (_parent: unknown, _args: unknown, context: Context): Promise<User | null> => {
+            if (context.user) {
+                return await User.findOne({ _id: context.user._id }).populate('savedBooks');
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        }
+    },
+    Mutations: {
+        // login a user using email or username
+        login: async (_parent: unknown, { email, password }: { email: string, password: string }): Promise<{ token: string, user: User }> => {
+            const user = await User.findOne({ $or: [{ username: email }, { email: email }] });
+            if (!user) {
+                throw new AuthenticationError(`Can't find this user`);
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Wrong password!');
+            }
+
+            const token = signToken(user.username, user.password, user._id);
+            return { token, user };
+        },
+
+        // create a new user
+        addUser: async (_parent: unknown, { input }: AddUserArgs): Promise<{ token: string, user: User }> => {
+            const user = await User.create({ ...input });
+            const token = signToken(user.username, user.password, user._id);
+            return { token, user };
+        },
+
+        saveBook: async (_parent: unknown, { bookData }: SaveBookArgs, context: Context): Promise<User | null> => {
+            if(context.user) {
+                return await User.findOneAndUpdate(
+                    { _id: context.user?._id },
+                    { $addToSet: { savedBooks: bookData } },
+                    { new: true, runValidators: true }
+                );
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+
+        removeBook: async (_parent: unknown, { bookId }: RemoveBookArgs, context: Context): Promise<User | null> => {
+            if(context.user) {
+                return await User.findOneAndUpdate(
+                    { _id: context.user?._id },
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true }
+                );
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        }
     }
 }
 
--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -1,8 +1,52 @@
 import { gql } from 'graphql-tag';
 
 const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input BookInput {
+        bookId: ID
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    input AddUserInput {
+        username: String!
+        email: String!
+        password: String!
+    }
+    
     type Query {
-        hello: String
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(input: AddUserInput!): Auth
+        saveBook(bookData: BookInput!): User
+        removeBook(bookId: ID!): User
     }
 `;
 
